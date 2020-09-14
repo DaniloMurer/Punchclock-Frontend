b1fdfde4de4f4df85364e128ec3e813b551ce14e
Danilo Jakob <danilo.murer@gmail.com>
2020-09-14 15:43:59 +0200

    CHANGE: Finished Project

--- src/app/dashboard/admin-dialog/update-category-dialog/update-category-dialog.component.ts
+++ src/app/dashboard/admin-dialog/update-category-dialog/update-category-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoryService} from '../../../service/category.service';
+import {Category} from '../../../data/category';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-update-category-dialog',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateCategoryDialogComponent implements OnInit {
 
-  constructor() { }
+  constructor(private categoryService: CategoryService) { }
+
+  dataSource: Category[];
+  displayedColumns: string[] = ['id', 'name'];
+  categoryId: FormControl = new FormControl('');
+  categoryName: FormControl = new FormControl('');
 
   ngOnInit(): void {
+    this.categoryService.getAllCategories().subscribe(data => {
+      this.dataSource = data;
+    });
+  }
+
+  public updateCategory(): void {
+    const category = new Category();
+    category.name = this.categoryName.value.toString();
+    // tslint:disable-next-line:radix
+    category.id = parseInt(this.categoryId.value);
+
+    this.categoryService.updateCategory(category).subscribe(data => {
+      console.log(data);
+    });
   }
 
 }
