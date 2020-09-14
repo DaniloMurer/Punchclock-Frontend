a6b741925707b6da926c34d57d6253eb048c288f
Danilo Jakob <danilo.murer@gmail.com>
2020-09-14 15:39:18 +0200

    CHANGE: Implemented delete category Dialog

--- src/app/dashboard/admin-dialog/delete-category-dialog/delete-category-dialog.component.ts
+++ src/app/dashboard/admin-dialog/delete-category-dialog/delete-category-dialog.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {CategoryService} from '../../../service/category.service';
 import {Category} from '../../../data/category';
+import {FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-delete-category-dialog',
@@ -13,10 +14,18 @@ export class DeleteCategoryDialogComponent implements OnInit {
 
   dataSource: Category[];
   displayedColumns: string[] = ['id', 'name'];
+  categoryId: FormControl = new FormControl('');
 
   ngOnInit(): void {
     this.categoryService.getAllCategories().subscribe(data => {
       this.dataSource = data;
+    });
+  }
+
+  public deleteCategory(): void {
+    // tslint:disable-next-line:radix
+    this.categoryService.deleteCategory(parseInt(this.categoryId.value)).subscribe(data => {
+      console.log(data);
     });
   }
 }
